71c207b8ccf386598042e1c815630fdbb435f572
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-07-15 19:19:50 +0800

    🌸

--- waybar/launch_waybar.sh
+++ waybar/launch_waybar.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-is_mpvpaper_ServerExist=`ps -ef|grep -m 1 mpvpaper|grep -v "grep"|wc -l`
-if [ "$is_mpvpaper_ServerExist" = "0" ]; then
-	echo "mpvpaper_server not found" > /dev/null 2>&1
-#	exit;
-elif [ "$is_mpvpaper_ServerExist" = "1" ]; then
-  killall mpvpaper
-fi
-
-is_swaybg_ServerExist=`ps -ef|grep -m 1 swaybg|grep -v "grep"|wc -l`
-if [ "$is_swaybg_ServerExist" = "0" ]; then
-	echo "swaybg_server not found" > /dev/null 2>&1
-#	exit;
-elif [ "$is_swaybg_ServerExist" = "1" ]; then
-  killall swaybg
-fi
-
 is_waybar_ServerExist=`ps -ef|grep -m 1 waybar|grep -v "grep"|wc -l`
 if [ "$is_waybar_ServerExist" = "0" ]; then
 	echo "waybar_server not found" > /dev/null 2>&1
