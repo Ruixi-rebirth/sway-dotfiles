2ff0d1ae9ad6eb934ed9dffb5e0c231978c317da
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-07-28 00:54:00 +0800

    dealt with some details

--- waybar/Cava-internal.sh
+++ waybar/Cava-internal.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
-
+is_cava_ServerExist=`ps -ef|grep -m 1 cava|grep -v "grep"|wc -l`
+if [ "$is_cava_ServerExist" = "0" ]; then
+	echo "cava_server not found" > /dev/null 2>&1
+#	exit;
+elif [ "$is_cava_ServerExist" = "1" ]; then
+  killall cava
+fi
 
 bar="▁▂▃▄▅▆▇█"
 dict="s/;//g;"
