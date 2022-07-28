413f5e4903f38d3b195234d4aa09eeadd901907a
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-07-28 08:53:19 +0800

    dealt with some details

--- rofi/off.sh
+++ rofi/off.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
-
-# This script defines just a mode for rofi instead of being a self-contained
-# executable that launches rofi by itself. This makes it more flexible than
-# running rofi inside this script as now the user can call rofi as one pleases.
-# For instance:
-#
-#   rofi -show powermenu -modi powermenu:./rofi-power-menu
-#
-# See README.md for more information.
-
 set -e
 set -u
 
--- rofi/powermenu.sh
+++ rofi/powermenu.sh
@@ -1,2 +1,2 @@
-
+#!/bin/bash
 rofi -show p -modi p:~/.config/rofi/off.sh -theme ~/.config/rofi/powermenu_theme.rasi
